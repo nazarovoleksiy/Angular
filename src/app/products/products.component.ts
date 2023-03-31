1dc227e230d5ea8bca5eba059eb2d1c1518715ac
Nazarov Oleksiy <112445970+nazarovoleksiy@users.noreply.github.com>
2023-03-31 16:53:01 +0300

    ngStyle

--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -42,7 +42,7 @@ export class ProductsComponent implements OnInit {
     },
     {
       "id": 3,
-      "available": 'Available',
+      "available": 'Not available',
       "title": "Mens Cotton Jacket",
       "price": 55.99,
       "description": "great outerwear jackets for Spring/Autumn/Winter, suitable for many occasions, such as working, hiking, camping, mountain/rock climbing, cycling, traveling or other outdoors. Good gift choice for you or your family member. A warm hearted love to Father, husband or son in this thanksgiving or Christmas Day.",
